659ff3d68234e65638d7540ad8e9a990ccaad804
Mehdi Valette <mehdi.valette@gmail.com>
2026-01-30 16:02:14 +0100

    ref: use private variables in binary-rtl

--- website/src/internal/static/resource/js/binary-rtl.js
+++ website/src/internal/static/resource/js/binary-rtl.js
@@ -4,74 +4,74 @@
 
 class VsBinaryRtl extends HTMLCanvasElement {
   /** @type {Array<{text: string, posY: number; interval: number | null}>} */
-  lines = [];
+  #lines = [];
 
   /** @type {CanvasRenderingContext2D | null} */
-  ctx = null;
+  #ctx = null;
 
-  lineHeight = 0;
-  charsPerLine = 0;
-  motionReduced = false;
+  #lineHeight = 0;
+  #charsPerLine = 0;
+  #motionReduced = false;
 
   connectedCallback() {
     this.style = "position: fixed; width: 100%; height: 100%; z-index: -1;";
-    this.ctx = this.getContext("2d");
+    this.#ctx = this.getContext("2d");
 
-    this.handleResize();
-    this.handleMotionReduce();
-    this.refreshAllLines();
+    this.#handleResize();
+    this.#handleMotionReduce();
+    this.#refreshAllLines();
   }
 
-  handleResize = () => {
+  #handleResize = () => {
     addEventListener("resize", () => {
-      this.setSizes();
-      this.refreshAllLines();
+      this.#setSizes();
+      this.#refreshAllLines();
     });
 
-    this.setSizes();
+    this.#setSizes();
   };
 
-  setSizes = () => {
-    if (this.ctx == null) return;
+  #setSizes = () => {
+    if (this.#ctx == null) return;
 
     this.width = this.clientWidth;
     this.height = this.clientHeight;
 
     // it looks like changing the width and height resets the context
-    this.ctx.font = "5rem mono";
-    this.ctx.fillStyle = "#ccf";
+    this.#ctx.font = "5rem mono";
+    this.#ctx.fillStyle = "#ccf";
 
-    this.charsPerLine =
-      (this.clientWidth / this.ctx.measureText("01").width) * 2;
+    this.#charsPerLine =
+      (this.clientWidth / this.#ctx.measureText("01").width) * 2;
 
-    this.lineHeight = this.ctx.measureText("01").emHeightAscent * 2;
+    this.#lineHeight = this.#ctx.measureText("01").emHeightAscent * 2;
   };
 
-  handleMotionReduce = () => {
+  #handleMotionReduce = () => {
     const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
 
     mediaQuery.addEventListener("change", () => {
-      this.motionReduced = mediaQuery.matches;
-      this.refreshAllLines();
+      this.#motionReduced = mediaQuery.matches;
+      this.#refreshAllLines();
     });
 
-    this.motionReduced = mediaQuery.matches;
+    this.#motionReduced = mediaQuery.matches;
   };
 
-  refreshAllLines = () => {
-    if (this.ctx == null) return;
+  #refreshAllLines = () => {
+    if (this.#ctx == null) return;
 
-    for (const l of this.lines)
+    for (const l of this.#lines)
       if (l.interval != null) clearInterval(l.interval);
 
-    this.lines = [];
+    this.#lines = [];
 
-    let posY = this.lineHeight;
+    let posY = this.#lineHeight;
 
-    for (let i = 0; posY <= this.clientHeight + this.lineHeight; i++) {
-      this.initLine(i, posY);
+    for (let i = 0; posY <= this.clientHeight + this.#lineHeight; i++) {
+      this.#initLine(i, posY);
 
-      posY += this.lineHeight;
+      posY += this.#lineHeight;
     }
   };
 
@@ -79,54 +79,53 @@ class VsBinaryRtl extends HTMLCanvasElement {
    * @param {number} lineIndex
    * @param {number} posY
    */
-  initLine = (lineIndex, posY) => {
+  #initLine = (lineIndex, posY) => {
     let text = "";
 
-    for (let i = 0; i < this.charsPerLine; i++) {
+    for (let i = 0; i < this.#charsPerLine; i++) {
       text += Math.random() > 0.5 ? "1" : "0";
     }
 
-    this.lines[lineIndex] = { text, posY, interval: null };
+    this.#lines[lineIndex] = { text, posY, interval: null };
 
-    if (!this.motionReduced) {
-      this.lines[lineIndex].interval = setInterval(
-        () => this.updateLine(lineIndex),
+    if (!this.#motionReduced) {
+      this.#lines[lineIndex].interval = setInterval(
+        () => this.#updateLine(lineIndex),
         Math.floor(Math.random() * 100 + 200),
       );
     }
 
-    this.drawLine(lineIndex);
+    this.#drawLine(lineIndex);
   };
 
   /**
    * @param {number} lineIndex
    */
-  updateLine = (lineIndex) => {
-    let { text, posY } = this.lines[lineIndex];
+  #updateLine = (lineIndex) => {
+    let { text, posY } = this.#lines[lineIndex];
 
-    this.lines[lineIndex].text =
+    this.#lines[lineIndex].text =
       text.substring(1) + (Math.random() > 0.5 ? "1" : "0");
 
-    this.drawLine(lineIndex);
+    this.#drawLine(lineIndex);
   };
 
   /**
    * @param {number} lineIndex
    */
-  drawLine = (lineIndex) => {
-    const { text, posY } = this.lines[lineIndex];
+  #drawLine = (lineIndex) => {
+    if (this.#ctx == null) return;
 
-    requestAnimationFrame(() => {
-      if (this.ctx == null) return;
+    const { text, posY } = this.#lines[lineIndex];
 
-      this.ctx.clearRect(
-        0,
-        posY - this.lineHeight,
-        this.clientWidth,
-        this.lineHeight + 10,
-      );
-      this.ctx.fillText(text, 10, posY);
-    });
+    this.#ctx.clearRect(
+      0,
+      posY - this.#lineHeight,
+      this.clientWidth,
+      this.#lineHeight + 10,
+    );
+
+    this.#ctx.fillText(text, 10, posY);
   };
 }
 
